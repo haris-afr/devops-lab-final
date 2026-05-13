2366b0649c01fd5f3eb27027055064797509cbc7
Haris Afridi <haris.afridi1312@gmail.com>
2026-05-13 02:31:57 -0400

    random3

--- api/app.py
+++ api/app.py
@@ -26,6 +26,7 @@ def get_course(cid):
         return jsonify({'error': 'Course not found'}), 404
     return jsonify(courses), 200
 
+#done
 @app.route('/api/courses', methods=['POST'])
 def post_course():
     data = request.get_json()
@@ -36,7 +37,7 @@ def post_course():
     courses.insert(data)
     return jsonify(courses), 200
 
-
+#done
 @app.route('/api/courses/<int:cid>', methods=['PUT'])
 def update_course(cid):
     course = next((c for c in courses if c['id'] == cid), None)
